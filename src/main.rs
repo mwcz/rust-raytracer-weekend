eda100a70311226f8a6c9c5a8290ae9049772b7b
mwcz <mwc@clayto.com>
2021-02-06 22:40:02 -0500

    Vec2<Float> addition implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use num::traits::Float;
+use std::fmt;
+use std::ops::{Add, AddAssign};
+
+struct Vec2<T: Float> {
+    x: T,
+    y: T,
+}
+
+impl<T: Float> Add for Vec2<T> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Vec2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl<T: Float> AddAssign for Vec2<T> {
+    fn add_assign(&mut self, other: Self) {
+        *self = Vec2 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl<T: fmt::Display + Float> fmt::Display for Vec2<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    for _i in 0..10 {
+        let a = Vec2 {
+            x: 100_f32,
+            y: 100_f32,
+        };
+        let b = Vec2 {
+            x: 200_f32,
+            y: 200_f32,
+        };
+        let c = a + b;
+        println!("{}", c);
+    }
 }
