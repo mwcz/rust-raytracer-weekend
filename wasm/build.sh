3504098e1b4e7e784750ec70492958b7704b2de1
mwcz <mwc@clayto.com>
2021-08-15 22:38:37 -0400

    re-enable minification

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,5 +1,4 @@
 wasm-pack build --release -t web
-# esbuild wasm-app.js wasm-worker.js --outdir=out --bundle --minify --splitting --format=esm --sourcemap=external && cp pkg/wasm_bg.wasm out
-esbuild rtw-render.js wasm-worker.js --outdir=dist --minify --bundle --splitting --format=esm --loader:.wasm=dataurl --sourcemap=external
+esbuild rtw-render.js wasm-worker.js --outdir=dist --bundle --minify --splitting --format=esm --sourcemap=external
 
 cp pkg/wasm_bg.wasm dist
