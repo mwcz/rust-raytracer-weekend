0797f4777ec6a2a0d71484ace7bd3f2bf28fb86a
mwcz <mwc@clayto.com>
2021-08-16 23:50:15 -0400

    use linked sourcemaps

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,5 +1,5 @@
 wasm-pack build --release -t web
-esbuild rtw-render.js wasm-worker.js --outdir=dist --bundle --minify --splitting --format=esm --sourcemap=external
+esbuild rtw-render.js wasm-worker.js --outdir=dist --bundle --minify --splitting --format=esm --sourcemap
 # esbuild rtw-render.js wasm-worker.js --outdir=dist --bundle --minify --splitting --format=esm --sourcemap=both
 
 cp pkg/wasm_bg.wasm dist
